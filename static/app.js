ae9d45742b0df21ea0f33db425a5973b0eb7dfce
rafe <rafe.estevez@gmail.com>
2021-06-20 03:15:26 -0400

    Added Bootstrap 5

--- static/app.js
+++ static/app.js
@@ -1,11 +1,11 @@
 const API_URL = "/todo"
 
 const INPUT = document.querySelector("#taskInput")
+const CATEGORY = document.querySelector("#categoryInput")
 const LIST = document.querySelector("#taskList")
 
 function sendToDo() {
-    let task = INPUT.value
-    let toDo = {id: "", task}
+    let toDo = {id: "", task: INPUT.value, completed: false, category: CATEGORY.value}
     fetch(API_URL, {
         method: 'POST',
         body: JSON.stringify(toDo),
@@ -16,17 +16,33 @@ function sendToDo() {
         .catch(error => console.error('Error:', error))
         .then(response => {
                 console.log('Success:', response);
-                LIST.innerHTML += `<li>${response.task}</li>`
-                INPUT.value = ""
+                addTodo(response)
+                clearInputs()
             }
         )
 }
 
-function getAll(){
-    fetch("/todos").then( response => response.json()).then(
+function addTodo(toDo) {
+    LIST.innerHTML += `
+            <li class="list-group-item d-flex justify-content-between align-items-center" id>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="${toDo.completed}">
+                </div>
+            ${toDo.task}
+            <span class="badge bg-primary rounded-pill">${toDo.category}</span>
+            </li>`
+}
+
+function clearInputs() {
+    INPUT.value = ""
+    CATEGORY.value = ""
+}
+
+function getAll() {
+    fetch("/todos").then(response => response.json()).then(
         data => {
             Array.from(data).forEach(
-                task => LIST.innerHTML += `<li>${task.task}</li>`
+                task => addTodo(task)
             )
         }
     )
